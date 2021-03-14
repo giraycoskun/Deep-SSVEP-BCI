9c22fd443209b84b98c124dd42f8b786431877f2
giraycoskun <giraycoskun@sabanciuniv.edu>
2021-03-14 23:22:56 +0300

    figures added

--- Analysis/plotAvgFFT.m
+++ Analysis/plotAvgFFT.m
@@ -5,8 +5,10 @@
 %}
 
 channel = 56;
-for target=1:5
-    subplot(5,1,target);
-    subjectAvgSignalFFT(channel, target, 1);
-    title('Single-Sided Amplitude Spectrum of X(t)');
+frequencyList = [15, 15.2, 15.4, 15.6, 15.8];
+targetList = [8,16,24,32,40];
+for index = 1:length(frequencyList)
+    subplot(5,1,index);
+    subjectAvgSignalFFT(channel, targetList(index), 1);
+    title(['Single-Sided Amplitude Spectrum of X(t), Character Index = ',num2str(targetList(index)), ', Flickering Frequency (Hz) = ', num2str(frequencyList(index))]);
 end
