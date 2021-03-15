b458b827826f20ebefd7361ccd96ace484267298
Deniz <dkucukahmetler@sabanciuniv.edu>
2021-03-16 00:12:16 +0300

    cca beta 2 saniyelik

--- Analysis/CCA_beta.m
+++ Analysis/CCA_beta.m
@@ -9,6 +9,7 @@
 
 %BETA PARAMETERS
 dataset = "BETA";
+supp_freqs = [8.60000000000000,8.80000000000000,9,9.20000000000000,9.40000000000000,9.60000000000000,9.80000000000000,10,10.2000000000000,10.4000000000000,10.6000000000000,10.8000000000000,11,11.2000000000000,11.4000000000000,11.6000000000000,11.8000000000000,12,12.2000000000000,12.4000000000000,12.6000000000000,12.8000000000000,13,13.2000000000000,13.4000000000000,13.6000000000000,13.8000000000000,14,14.2000000000000,14.4000000000000,14.6000000000000,14.8000000000000,15,15.2000000000000,15.4000000000000,15.6000000000000,15.8000000000000,8,8.20000000000000,8.40000000000000];
 subban_no = 3;
 signal_length = 1;
 totalsubject=70;
@@ -17,7 +18,7 @@ totalcharacter=40;
 sampling_rate=250;
 visual_latency=0.13;
 visual_cue=0.5;
-sample_length=sampling_rate*signal_length; 
+%sample_length=sampling_rate*signal_length; 
 total_ch=64;
 max_epochs=800;
 dropout_second_stage=0.7;
@@ -26,11 +27,10 @@ dropout_second_stage=0.7;
 
 total_delay=visual_latency+visual_cue; % Total undesired signal length in seconds
 delay_sample_point=round(total_delay*sampling_rate); % # of data points correspond for undesired signal length
-sample_interval = (delay_sample_point+1):delay_sample_point+sample_length; % Extract desired signal
 channels=[48 54 55 56 57 58 61 62 63];% Indexes of 9 channels: (Pz, PO3, PO5, PO4, PO6, POz, O1, Oz, and O2)
 % To use all the channels set channels to 1:total_ch=64;
 
-[AllData,y_AllData]=PreProcess(channels,sample_length,sample_interval,subban_no,totalsubject,totalblock,totalcharacter,sampling_rate,dataset);
+%[AllData,y_AllData]=PreProcess(channels,sample_length,sample_interval,subban_no,totalsubject,totalblock,totalcharacter,sampling_rate,dataset);
 
 %% -AllData: Preprocessing the data with bandpass filter/s,
 %					Dimension of AllData:
@@ -39,15 +39,17 @@ channels=[48 54 55 56 57 58 61 62 63];% Indexes of 9 channels: (Pz, PO3, PO5, PO
 %         -y_AllData: Labels of characters in AllData					
 
 %% CCA
-%accuracy plot
-samp_pts = (50:25:250);
-%accs = [];
+samp_pts = (50:25:500);
 ave_accs = [];
 
+signal_lengths = samp_pts./250;
 for idx = 1:length(samp_pts)
- 
-    T = samp_pts(idx);
     
+    sample_length = signal_lengths(idx)*sampling_rate;
+    sample_interval = (delay_sample_point+1):delay_sample_point+sample_length;
+    [AllData,y_AllData]=PreProcess(channels,sample_length,sample_interval,subban_no,totalsubject,totalblock,totalcharacter,sampling_rate,dataset);
+
+    T = samp_pts(idx);
     S = sampling_rate; %sampling rate = 246
     t = linspace(1/S, T/S, T); %t = 1/S, 2/S, ... , T/S
 
@@ -94,10 +96,8 @@ for idx = 1:length(samp_pts)
 end
 %% Plot
 disp(ave_accs);
-bar(ave_accs);
-title("CCA")
+bar( (signal_lengths),ave_accs);
+%title("CCA")
 xlabel('Time (s)')
-xticklabels(samp_pts./250);
-yticks((0.0:0.05:0.45));
 ylabel('Accuracy (%)')
 grid on
